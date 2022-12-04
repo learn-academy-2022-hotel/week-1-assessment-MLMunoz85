8f3eacf8da35548092154acc0b020c8c28aae4cf
MLMunoz85 <matthew.l.munoz@gmail.com>
2022-12-04 11:23:41 -0800

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -6,63 +6,180 @@
 
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in two strings and determines which of the two strings has more characters. Use the two sets of test variables provided.
 
-// Pseudo code:
+// Function Signature
+// input: strings √
+// output: string indicating which of the two strings has more characters √
+
+// input: "apple", "banana"
+// output: "Banana has more characters than apple"
+
+// input: "cherry", "kiwi"
+// output: "Cherry has more characters than kiwi"
+
+// Process √
+// create a function called bigFruit that takes in a set of strings called fruit1, fruit2 and fruit3, fruit4
+// if fruit1 has more characters than fruit2 return fruit1 has more characters than fruit2
+// any other condition return fruit2 has more characters than fruit1
+// if fruit3 has more characters than fruit4 return fruit3 has more characters than fruit4
+// any other condition return fruit4 has more characters than fruit3
+// string interpolation for the return statement
+
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-// Expected outcome: "banana"
+// const fruit1 = "apple"
+// @@ -18,6 +36,31 @@ const fruit3 = "cherry"
+// const fruit4 = "kiwi"
+// // Expected outcome: "cherry"
 
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
-// Expected outcome: "cherry"
+// // Function
+// const bigFruit = (value1, value2) => {
+//   if(value1.length > value2.length){
+//     return `${value1} has more characters than ${value2}`
+//   } else if(value1.length === value2.length) {
+//     return `${value1} has the same amount of characters as ${value2}`
+//   } else {
+//     return `${value2} has more characters than ${value1}`
+//   }
+// }
+
+// console.log(bigFruit(fruit1, fruit2))
+// output: banana has more characters than apple
+
+// console.log(bigFruit(fruit3, fruit4))
+// output:cherry has more characters than kiwi
+
+// console.log(bigFruit(fruit2, fruit3))
+// output: banana has the same amount of characters as cherry
+
+// console.log(bigFruit("watermelon", "grape"))
+// output: watermelon has more characters than grape
 
 // --------------------1) Create a function that takes in a number and determines if the given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Pseudo code:
 
+// Function signature
+// input: numbers
+// output: it will show the desired output based on the number that is input
+
+// input: 42
+// outcome: "42 is below boiling point"
+
+// input: 350
+// output: "350 is above boiling point"
+
+// input: 212
+// output: "212 is at boiling point"
+
+// Process:
+// Create a function called waterTemp that takes in a set of numbers called temp1, temp2, and temp3. 
+// Will create an argument called value and when you enter one of the three temps will produce one of the three outcomes in a string.
+// Will use the following conditional statements:
+// 1) If the number is 212 the outcome will be "212 is at boiling point"
+// 2) If the number is higher than 212 then the outsome will be "'value' above boiling point"
+// 3) An else condtion to catch all numbers lower than 212 that will produce the outcome "'value' is below boiling point"
+
+const waterTemp = (value) => {
+    if (value === 212) {
+        return `${value} is at boiling point`
+    } else if (value > 212) {
+        return `${value} is above boiling point`
+    } else {
+        return `${value} is below boiling point`
+    }
+}
 const temp1 = 42
 // Expected output: "42 is below boiling point"
+// console.log(waterTemp(temp1))
+// outcome: "42 is below boiling point"
 
 const temp2 = 350
 // Expected output: "350 is above boiling point"
+// console.log(waterTemp(temp2))
+// outcome: "350 is below boiling point"
 
 const temp3 = 212
 // Expected output: "212 is at boiling point"
+// console.log(waterTemp(temp3))
+// output: "212 is at boiling point"
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Pseudo code:
+// Function signature
+// input: arrays (numbers)
+// output: the code will combine the two arrays and will return the length of the variables.
+
+// Process: I am going to create a code that makes a variable called padresRuns that combines the two arrays into one with concat. Then I will use the .length to get the length of the variables within the new combined array.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
+
+var padresRuns = padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns)
 // Expected output: 9
+// console.log(padresRuns.length)
+// outcome: 9
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Pseudo code:
+// Function signature
+// input: string
+// output: the output should be the string "Hotel 2022" and getting it to reverse the letters into "2202 letoH"
+
+// Process: I want to create a variable that takes the string "Hotel 2022" using .split and turns it into an array. Once I have done that I am going to create another variable that will reverse the order of the array using the reverse mutator. After, I will turn it back into a string with join.
 
 const currentCohort = "Hotel 2022"
+
+var splitHotel = currentCohort.split("")
+var reverseHotel = splitHotel.reverse()
+
+// console.log(reverseHotel.join(""))
+// outcome: "2202 letoH"
 // Expected output: "2202 letoH"
 
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Pseudo code:
+// Function Signature
+// input: number
+// output: the output should be the last index of the number that is input from the array.
+
+// Process: I will utilize the accessor .lastIndexOf to create a function called lastIndex that when I input a value from the array it will return the last index of that value.
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
+
+const lastIndex = (value) => {
+    return myNumbers.lastIndexOf(value)
+}
 const givenValue1 = 42
+// console.log(lastIndex(givenValue1))
+// output: 7
 // Expected output: 7
 
 const givenValue2 = 10
+// console.log(lastIndex(givenValue2))
+// output: 8
 // Expected output: 8
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Pseudo code:
+// Function signature
+// input: number (array)
+// output: the output should sort all of the numbers in an array from largest to smallest
 
+// Process: I will create a function called tempsSort that will use the built in method sort() to sort the numbers. Because sort turns the values of an array into a string to sort, with numbers it will be in order of first number. So I need to use a different code line to get them in the proper order from largest to smallest.
+
+const tempSort = (value) => {
+    return value.sort(function(a, b){return b-a})
+}
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
+// console.log(tempSort(sanDiegoSummerTemperatures))
+// output: [82, 80, 79, 77, 76, 73, 72]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
+// console.log(tempSort(sanDiegoWinterTemperatures))
+// outcome: [68, 67, 66, 66, 62, 59, 59]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
--- interview-questions.md
+++ interview-questions.md
@@ -14,44 +14,48 @@ Researched answer:
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer: Git is a version control system that allows you to store code on your local system. Git is accessed through the terminal in your computer. GitHub allows you to store these different versions of code in the GitHub cloud in different repositories.
 
-Researched answer:
+Researched answer: Git is a high- quality version control system that is installed and maintained on your local system. The big thing with Git is its branching model. Branching allows you to create independent local branches of your code. GitHub is a Git repository hosting service that stores repositories in the cloud. With GitHub, branching let's you can try out new ideas or make changes to the code without affecting the main source code until you're actually ready to push your branch and merge your code to the main.
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer: The JavaScript operators that return a Boolean value are Equality operators and Logical operators. They both set conditions for whether something is true or false.
 
-Researched answer:
+Researched answer: The JavaScript operators that return a Boolean value are Equality, Relational, and Logical operators. Equality operators compare one value to the other and judge if the values equate the same thing, a Boolean value is returned. Relational operators look for a a relationship between two values, a Boolean value is returned. Logical operators (and, or, not), take two complete statements and chain them together, a Boolean operator is returned.
 
 3. What is an index? What is the difference between index and value?
 
-Your answer:
+Your answer: An index is the location of a certain character/value in a string or an array. The difference between index and value is the index is the position where the value is located. The value can be string, name, number etc.
 
-Researched answer:
+Researched answer: An index is the location of a certain character/value in a string or an array. The difference between index and value is the index is the position where the value is located. The value can be string, character, number etc, as long as it is a JavaScript data type. For example if I have an array ["Matt", "David", "Chris", "Allen"], each name or string is a value. Each of the values have an index: Matt is 0, David is 1, Chris is 2, Allen is 3.
 
 4. Compare and contrast arrays and strings. Please include ways in which they are the same and ways in which they are different.
 
-Your answer:
+Your answer: Strings are characters such as letters and words. Arrays are where data like strings and numbers are grouped into. Strings can have characters, while Arrays can have strings, numbers, and characters. 
 
-Researched answer:
+Researched answer: Strings are a collection of characters wrapped in quotation marks. Strings can contain any type of characters and as many characters as needed. There are many properties and operations that can be applied to strings, one is concatenation, which is the joining of multiple strings. Arrays are collections of ordered data. Arrays can contain any type of information as long as it's a data type JavaScript recognizes. Strings are contained within quotation marks, while Arrays are contained within closed square brackets.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-Your answer:
+Your answer: Pair programming is when two people are working on lines of code together. The driver is the person behind the keyboard who is writing the code. The navigator is the person watching the driver's screen, it is their job to support the driver by helping them with the code and helping catch errors in the code.
 
-Researched answer:
+Researched answer: When pair programming it is important that you take a few minutes to read through and understand the objective of the challenge. Come up with a plan on how you want to appraoch the challenge and accomplish the goal at hand. When pair programming there is two jobs: the driver and the navigator. The driver is the one with their hands on the keyboard. They are responsible for entering information and following the instructions given by the naviator. This approach takes constant communication and talking about your code will make you a better developer and a better teammate.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: In JavaScript, functions can be assigned to variables in the same way that strings or arrays can. They can be passed into other functions as parameters or returned to them as well. A higher-order function is a function that accepts functions as parameters and/or returns a function.
 
-2. Jest:
+2. Jest: Jest is a JavaScript testing framework designed to ensure the correctness of any JavaScript codebase. It allows you to write tests with an approachable, familiar, and feature-rich application programming interface that gives results quickly.
 
-3. Objects:
+3. Objects: JavaScript variables are containers for data values, objects are variables too but can contain many values. An object is an unordered collection of key-value pairs. Each key-value pair is called a property. The key of a property can be a string, and the value of a property can be any value e.g., a string, a number, an arry, and even a function. The name:values pairs in objects are called properties.Objects can also have methods, which are actions that can be performed on objects.
 
-4. Method:
+4. Method: JavaScript methods are actions that can be performed on objects. A method is a function stored as a property, they are stored in properties as function definitions. For example: Line 58 is the method.
+    const person(object) = {
+        name: "John".
+        greet: function() { console.log("hello)}
+    }
 
-5. Classes:
+5. Classes: Classes are bits of code that encompass multiple objects, methods, and allow manipulation for its member variables and functions. Within each language, a class has different syntax and the same holds true for JavaScript. In JavaScript, a class is simply a variant of functions. Using the class functionality, you can define the constructor and the prototype functions for the member objects in one encompassing block.
